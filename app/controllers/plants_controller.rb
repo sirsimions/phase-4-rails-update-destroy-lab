b17df7b2ecc397e69847c96a43767f7498211e46
Simion <simion.okola@student.moringaschool.com>
2022-11-24 08:45:24 +0300

    Completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+#DESTROY plant
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  plant.destroy
+  head :no_content
+end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
   end
 end
